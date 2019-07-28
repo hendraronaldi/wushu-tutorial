77112422de99c2b82c17abe557e82c52c295de8d
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-28 15:04:54 +0700

    dark background color

--- src/views/Courses.vue
+++ src/views/Courses.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+        <base-header type="gradient-dark" class="pb-6 pb-8 pt-5 pt-md-8">
         </base-header>
 
         <div class="container-fluid mt--7">
