3e52d462cdbedc77405e751b36b5f63e1efbd0fa
hendraronaldi <hendraronaldi@hotmail.com>
2019-07-30 14:48:43 +0700

    store getters

---
+++ src/store/getters.js
@@ -0,0 +1,11 @@
+export const currentCategory = state => {
+    return state.currentCategory
+}
+
+export const currentLevel = state => {
+    return state.currentLevel
+}
+
+export const currentCourse = state => {
+    return state.currentCourse
+}
