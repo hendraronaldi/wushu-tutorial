94f37328f91c6d99f9b6507d5146e5031fde8833
hendraronaldi <hendraronaldi@hotmail.com>
2019-08-03 13:32:39 +0700

    user layout

---
+++ src/layout/user/UserDashboardLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <side-bar
+      :background-color="sidebarBackground"
+      short-title="Argon"
+      title="Argon"
+    >
+      <template slot="links">
+        
+        <sidebar-item :link="{name: 'Courses', icon: 'ni ni-hat-3 text-success', path: '/courses'}"/>
+        <sidebar-item :link="{name: 'User Profile', icon: 'ni ni-single-02 text-yellow', path: '/profile'}"/>
+
+      </template>
+    </side-bar>
+    <div class="main-content" :data="sidebarBackground">
+      <dashboard-navbar></dashboard-navbar>
+
+      <div @click="toggleSidebar">
+        <fade-transition :duration="200" origin="center top" mode="out-in">
+          <!-- your content here -->
+          <router-view></router-view>
+        </fade-transition>
+        <content-footer v-if="!$route.meta.hideFooter"></content-footer>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import DashboardNavbar from './DashboardNavbar.vue';
+  import ContentFooter from './ContentFooter.vue';
+  import { FadeTransition } from 'vue2-transitions';
+
+  export default {
+    components: {
+      DashboardNavbar,
+      ContentFooter,
+      FadeTransition
+    },
+    data() {
+      return {
+        sidebarBackground: 'vue' //vue|blue|orange|green|red|primary
+      };
+    },
+    methods: {
+      toggleSidebar() {
+        if (this.$sidebar.showSidebar) {
+          this.$sidebar.displaySidebar(false);
+        }
+      }
+    }
+  };
+</script>
+<style lang="scss">
+</style>
