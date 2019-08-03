7cb6d4b236d7f2c2e058ab3800ff0c5707bedb13
hendraronaldi <hendraronaldi@hotmail.com>
2019-08-03 08:42:50 +0700

    landing page view

---
+++ src/views/Landing.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+
+        <div class="position-relative">
+            <!-- shape Hero -->
+            <section class="section-shaped my-0">
+                <div class="shape shape-style-1 shape-default shape-skew" style="background:black">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6 text-center">
+                                <img src="img/brand/TP_01_Med.png" alt=""
+                                    style="height:auto; max-width: 250px;"
+                                >
+                            </div>
+                            <div class="col-lg-6 text-center">
+                                <h1 class="display-3 text-white">Wushu Teratai Putih
+                                    <span>北少林, 白莲们</span>
+                                    <span>Bandung, Indonesia</span>
+                                </h1>
+                                <p class="lead  text-white">Tuesday & Thursday 19.00-21.00</p>
+                                <p class="lead  text-white">Saturday 15.00-18.00 </p>
+                                <p class="lead  text-white">@ Sekolah Kristen Gamaliel, Jl Jendral Sudirman No. 205 - 207</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
