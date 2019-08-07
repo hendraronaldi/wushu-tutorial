2da9740ee2bc1169f6bf37b05c24d838b87b1951
hendraronaldi <hendraronaldi@hotmail.com>
2019-08-07 07:22:12 +0700

    production settings

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 const webpack = require('webpack');
-// const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  // publicPath: isProd ? "/vue-argon-dashboard/" : "",
+  publicPath: isProd ? "/vue-argon-dashboard/" : "",
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
@@ -20,6 +20,6 @@ module.exports = {
   },
   css: {
     // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
+    sourceMap: process.env.NODE_ENV === 'production'
   }
 };
