61ca2e71b21ba3ccb358d5af4ffc179eaac06b0f
hendraronaldi <hendraronaldi@hotmail.com>
2019-08-09 10:53:15 +0700

    remove unused data, change labels

--- src/views/Dashboard/SocialTrafficTable.vue
+++ src/views/Dashboard/SocialTrafficTable.vue
@@ -14,7 +14,7 @@
         <template slot="columns">
           <th>Details</th>
           <th>Score</th>
-          <th>Changes</th>
+          <th>Growth</th>
         </template>
 
         <template slot-scope="{row}">
@@ -52,43 +52,6 @@
     props: {
       performanceData: Object
     },
-
-    data() {
-      return {
-        tableData: [
-          {
-            name: 'Facebook',
-            visitors: '1,480',
-            progress: 60,
-            progressType: 'gradient-danger',
-          },
-          {
-            name: 'LinkedIn',
-            visitors: '5,480',
-            progress: 70,
-            progressType: 'gradient-success',
-          },
-          {
-            name: 'Google',
-            visitors: '4,807',
-            progress: 80,
-            progressType: 'gradient-primary',
-          },
-          {
-            name: 'Instagram',
-            visitors: '3,678',
-            progress: 75,
-            progressType: 'gradient-info',
-          },
-          {
-            name: 'Twitter',
-            visitors: '2,645',
-            progress: 30,
-            progressType: 'gradient-warning',
-          }
-        ]
-      }
-    }
 
   }
 </script>
