1b4afd99c0578b5bb41ff1baa25a62a584bd3a60
hendraronaldi <hendraronaldi@hotmail.com>
2019-08-18 14:34:52 +0700

    performance data skeleton

---
+++ src/api/performance/performance.js
@@ -0,0 +1,18 @@
+export const userPerformanceData = {
+    email: String,
+    date: String,
+    flexibility: {
+		shoulder: Number,
+		wrist: Number,
+		waist: Number,
+		leg: Number
+	},
+	power: {
+		strike: Number,
+		handswing: Number,
+		spin: Number,
+		jump: Number,
+		kick: Number,
+		legswing: Number
+	}
+};
